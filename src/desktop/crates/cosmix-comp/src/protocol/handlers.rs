e74d89b60ceb874cabee6111489a0b97a0e27594
Mark Constable <markc@renta.net>
2026-09-17 20:59:53 +1000

    Revert "TEMP falsify G3 test (reverted next)"
    This reverts commit b93cc57ecb2df0dfdc76296e04edb51ad86e5f41.

--- src/desktop/crates/cosmix-comp/src/protocol/handlers.rs
+++ src/desktop/crates/cosmix-comp/src/protocol/handlers.rs
@@ -80,7 +80,7 @@ impl CompositorHandler for WaylandState {
         compositor::add_pre_commit_hook::<WaylandState, _>(surface, |state, _, surface| {
             state.prepare_scene_commit(surface);
             state.prepare_acquire_gate(surface);
-            let _ = &state;
+            state.stage_presentation_feedback(surface);
         });
         self.surface_count = self.surface_count.saturating_add(1);
         let _ = Self::with_client_state(surface, |client_state| {
